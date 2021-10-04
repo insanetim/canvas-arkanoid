f62063314408b3daaeb65ede3bcf7eccc5d488e0
insanetim <47120724+insanetim@users.noreply.github.com>
2021-10-04 21:24:32 +0300

    Update game.js

--- game.js
+++ game.js
@@ -65,10 +65,19 @@ const game = {
   update() {
     this.platform.move();
     this.ball.move();
+    this.collideBlocks();
+    this.collidePlatform();
+  },
+  collideBlocks() {
     for (let block of this.blocks) {
       if (this.ball.collide(block)) {
         this.ball.bumpBlock(block);
       }
+    }
+  },
+  collidePlatform() {
+    if (this.ball.collide(this.platform)) {
+      this.ball.bumpPlatform(this.platform);
     }
   },
   run() {
@@ -146,8 +155,13 @@ game.ball = {
       return false;
     }
   },
-  bumpBlock(element) {
+  bumpBlock(block) {
     this.dy *= -1;
+  },
+  bumpPlatform(platform) {
+    this.dy *= -1;
+    let touchX = this.x + this.width / 2;
+    this.dx = this.velocity * platform.getTouchOffset(touchX);
   },
 };
 
@@ -156,6 +170,8 @@ game.platform = {
   dx: 0,
   x: 280,
   y: 300,
+  width: 100,
+  height: 14,
   ball: game.ball,
   fire() {
     if (this.ball) {
@@ -180,6 +196,12 @@ game.platform = {
         this.ball.x += this.dx;
       }
     }
+  },
+  getTouchOffset(x) {
+    let diff = this.x + this.width - x;
+    let offset = this.width - diff;
+    let result = (2 * offset) / this.width;
+    return result - 1;
   },
 };
 
