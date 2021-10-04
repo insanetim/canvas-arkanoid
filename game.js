3aacd933f4d42ed83b8c965ef12cac6811b7055f
insanetim <47120724+insanetim@users.noreply.github.com>
2021-10-04 17:33:14 +0300

    Update game.js

--- game.js
+++ game.js
@@ -54,6 +54,8 @@ const game = {
     for (let row = 0; row < this.rows; row++) {
       for (let col = 0; col < this.cols; col++) {
         this.blocks.push({
+          width: 60,
+          height: 20,
           x: 64 * col + 65,
           y: 24 * row + 35,
         });
@@ -63,6 +65,11 @@ const game = {
   update() {
     this.platform.move();
     this.ball.move();
+    for (let block of this.blocks) {
+      if (this.ball.collide(block)) {
+        this.ball.bumpBlock(block);
+      }
+    }
   },
   run() {
     window.requestAnimationFrame(() => {
@@ -124,6 +131,23 @@ game.ball = {
     if (this.dx) {
       this.x += this.dx;
     }
+  },
+  collide(element) {
+    let x = this.x + this.dx;
+    let y = this.y + this.dy;
+    if (
+      x + this.width > element.x &&
+      x < element.x + element.width &&
+      y + this.height > element.y &&
+      y < element.y + element.height
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  },
+  bumpBlock(element) {
+    this.dy *= -1;
   },
 };
 
