e264a78a3d93ac37186e653d6d8aa326eb8c4801
insanetim <47120724+insanetim@users.noreply.github.com>
2021-10-06 17:52:48 +0300

    Update game.js

--- game.js
+++ game.js
@@ -5,6 +5,7 @@ const KEYS = {
 };
 
 const game = {
+  running: true,
   ctx: null,
   platform: null,
   ball: null,
@@ -84,11 +85,13 @@ const game = {
     }
   },
   run() {
-    window.requestAnimationFrame(() => {
-      this.update();
-      this.render();
-      this.run();
-    });
+    if (this.running) {
+      window.requestAnimationFrame(() => {
+        this.update();
+        this.render();
+        this.run();
+      });
+    }
   },
   render() {
     this.ctx.clearRect(0, 0, this.width, this.height);
@@ -185,7 +188,9 @@ game.ball = {
       this.y = 0;
       this.dy = this.velocity;
     } else if (ballBottom > worldBottom) {
-      console.log("you lose");
+      game.running = false;
+      alert("You lose!");
+      window.location.reload();
     }
   },
   bumpBlock(block) {
